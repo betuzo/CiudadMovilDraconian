a389c01659c5cd6fc005b2d05e9c5eb6b08925ca
betuzo <betotsol@gmail.com>
2012-09-07 17:59:59 -0500

    Initial Commit

---
+++ CiudadMovilDraconianTests/CiudadMovilDraconianTests.h
@@ -0,0 +1,13 @@
+//
+//  CiudadMovilDraconianTests.h
+//  CiudadMovilDraconianTests
+//
+//  Created by Mobiik on 07/09/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CiudadMovilDraconianTests : SenTestCase
+
+@end
---
+++ CiudadMovilDraconianTests/CiudadMovilDraconianTests.m
@@ -0,0 +1,32 @@
+//
+//  CiudadMovilDraconianTests.m
+//  CiudadMovilDraconianTests
+//
+//  Created by Mobiik on 07/09/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CiudadMovilDraconianTests.h"
+
+@implementation CiudadMovilDraconianTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CiudadMovilDraconianTests");
+}
+
+@end
